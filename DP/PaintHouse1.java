8eee1dcbf48766f504ab17647d628045109ddcfb
JanhaviItankar12 <itankarjanvi@gmail.com>
2026-04-22 17:53:50 +0530

    paintHousespace optimized

--- DP/PaintHouse1.java
+++ DP/PaintHouse1.java
@@ -9,23 +9,27 @@ public class PaintHouse1 {
         int dp[][]=new int[n][3];
 
         //base case
-        dp[0][0]=cost[0][0];  //red color
-        dp[0][1]=cost[0][1];  //blue color
-        dp[0][2]=cost[0][2];  //green color
+        int prevRed=cost[0][0];  //red color
+        int prevBlue=cost[0][1];  //blue color
+        int prevGreen=cost[0][2];  //green color
 
         for(int i=1;i<n;i++){
 
             //red color
-            dp[i][0]=cost[i][0]+Math.min(dp[i-1][1],dp[i-1][2]);
+            dp[i][0]=cost[i][0]+Math.min(prevBlue,prevGreen);
 
             //blue color
-            dp[i][1]=cost[i][1]+Math.min(dp[i-1][0],dp[i-1][2]);
+            dp[i][1]=cost[i][1]+Math.min(prevRed,prevGreen);
 
             //red color
-            dp[i][2]=cost[i][2]+Math.min(dp[i-1][1],dp[i-1][0]);
+            dp[i][2]=cost[i][2]+Math.min(prevRed,prevBlue);
+
+            prevRed=dp[i][0];
+            prevBlue=dp[i][1];
+            prevGreen=dp[i][2];
         }
 
-        System.out.println(Math.min(dp[n-1][0],Math.min(dp[n-1][1],dp[n-1][2])));
+        System.out.println(Math.min(prevRed,Math.min(prevBlue,prevGreen)));
         
     }
 }
